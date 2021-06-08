c413790fc3acc5f7fddee64f8b80336517be76e3
Daniel <daniel.laskowski94@gmail.com>
2021-06-08 13:48:45 +0200

    update desserts amount

--- src/components/DessertGenerator.js
+++ src/components/DessertGenerator.js
@@ -1,18 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import CountUp from 'react-countup';
-import { fetchtTableLength, fetchDessert } from '../utils/fetchData'
+import { fetchtTableLength, fetchDessert } from '../utils/fetchData';
 
 export default function DessertGenerator() {
-
   const [dessert, setDessert] = useState({
     recordId: '',
     number: '',
     name: '',
     img: '',
     meta: '',
-  })
+  });
 
-  const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * (705 - 1 + 1)) + 1);
+  const [randomNumber, setRandomNumber] = useState(
+    Math.floor(Math.random() * (497 - 1 + 1)) + 1
+  );
   const [showDessert, setShowDessert] = useState(false);
   const [showTaken, setShowTaken] = useState(false);
   const [buttonDisabled, setButtonDisabled] = useState(false);
@@ -21,12 +22,12 @@ export default function DessertGenerator() {
 
   useEffect(() => {
     getTableLength();
-  }, [])
+  }, []);
 
   const getTableLength = async () => {
     const data = await fetchtTableLength();
     setRandomNumber(data);
-  }
+  };
 
   const getRandomDessert = async () => {
     getTableLength();
@@ -34,11 +35,11 @@ export default function DessertGenerator() {
     // Reset previous state values
     setDessert({});
     setShowDessert(false);
-    setShowTaken(false)
+    setShowTaken(false);
 
     // Draw a random number
     //setRandomNumber(Math.floor(Math.random() * (tabLength - 1 + 1)) + 2);
-    console.log(randomNumber)
+    console.log(randomNumber);
 
     const dessert = await fetchDessert(randomNumber);
     setDessert(dessert);
@@ -49,36 +50,47 @@ export default function DessertGenerator() {
       } else {
         setShowTaken(true);
       }
-      setButtonDisabled(false)
+      setButtonDisabled(false);
     }, DURATION);
-  }
+  };
 
   return (
     <main className="main-dessert">
       <div className="card">
         <h1>Jakim deserem dziś jesteś?</h1>
-        {dessert.name
-          ? (
-            <CountUp className="countup-dessert" end={dessert.number} duration={DURATION / 1000} />
-          )
-          : (
-            <span className="time">?</span>
-          )}
+        {dessert.name ? (
+          <CountUp
+            className="countup-dessert"
+            end={dessert.number}
+            duration={DURATION / 1000}
+          />
+        ) : (
+          <span className="time">?</span>
+        )}
         {showTaken && <h3>Deser o tym numerze jest już zajęty!</h3>}
 
-
-        <button disabled={buttonDisabled} onClick={getRandomDessert}>{showTaken ? `Spróbuj ponownie` : `Losuj`}</button>
+        <button disabled={buttonDisabled} onClick={getRandomDessert}>
+          {showTaken ? `Spróbuj ponownie` : `Losuj`}
+        </button>
       </div>
-      {showDessert
-        && dessert.meta !== 'taken' && (
-          <div className="card">
-            <h1>{dessert.name}</h1>
-            <img className="img-dessert" src={dessert.img} alt={dessert.name} />
-            <p className='meta-dessert' dangerouslySetInnerHTML={{ __html: dessert.meta }} />
-            <a className="button" href={`https://en.wikipedia.org/wiki/${dessert.name}`} target='_blank' rel="noreferrer">Read More</a>
-          </div>
-        )}
+      {showDessert && dessert.meta !== 'taken' && (
+        <div className="card">
+          <h1>{dessert.name}</h1>
+          <img className="img-dessert" src={dessert.img} alt={dessert.name} />
+          <p
+            className="meta-dessert"
+            dangerouslySetInnerHTML={{ __html: dessert.meta }}
+          />
+          <a
+            className="button"
+            href={`https://en.wikipedia.org/wiki/${dessert.name}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Read More
+          </a>
+        </div>
+      )}
     </main>
-  )
+  );
 }
-
